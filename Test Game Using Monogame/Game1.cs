0f03533be2e335aabb96a65cdf913b8d73017cc5
Jacob Garm <jacobgarm@hotmail.dk>
2022-02-26 11:41:06 +0100

    Add project files.

---
+++ Test Game Using Monogame/Game1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Test_Game_Using_Monogame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D playerFace;
+        Vector2 position = Vector2.Zero;
+        int speed = 4;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            playerFace = this.Content.Load<Texture2D>("t");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || state.IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (state.IsKeyDown(Keys.A))
+                position.X -= speed;
+
+            if (state.IsKeyDown(Keys.D))
+                position.X += speed;
+
+            if (state.IsKeyDown(Keys.W))
+                position.Y -= speed;
+
+            if (state.IsKeyDown(Keys.S))
+                position.Y += speed;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Bisque);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(playerFace, position, color: Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Test Game Using Monogame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test_Game_Using_Monogame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
